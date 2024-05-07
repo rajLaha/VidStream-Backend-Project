24beb405e8159b03ef91942a04b81890483372bf
rajLaha <priyanshulaha@gmail.com>
2024-05-07 23:51:53 +0530

    add cloudinary utility for file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import fs from "fs";
+import { loadEnvFile } from "process";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(loadEnvFile, {
+      resource_type: "auto",
+    });
+    //   file has been upload succesfully
+    console.log(`file is uploaded on cloudinary ${response.url}`);
+    return response;
+  } catch (error) {
+    fs.unlink(localFilePath);
+    // remove the locally saved temporary as the uploaded operation gets failed
+    return null;
+  }
+};
+
+cloudinary.uploader.upload(
+  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) {
+    console.log(result);
+  }
+);
