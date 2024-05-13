a9967ba011b232d3bdd55c4b789f4b3c8463b5f2
rajLaha <priyanshulaha@gmail.com>
2024-05-13 23:57:49 +0530

    .

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -28,6 +28,6 @@ router.route("/login").post(loginuser);
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutuser);
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
