4df6c791836ec7ece3a479aac956bf918af663b0
rajLaha <priyanshulaha@gmail.com>
2024-05-16 20:55:02 +0530

    fixing bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -216,7 +216,7 @@ const logoutuser = asyncHandler(async (req, res) => {
 
 // Refresh Access Token code
 
-const refreshAccessToken = asyncHandler(async (req, res) => {
+fconst refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken =
     req.cookies.refreshToken || req.body.refreshToken;
 
