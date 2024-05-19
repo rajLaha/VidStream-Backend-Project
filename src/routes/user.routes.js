d2de8788b2e980561d0494af2dd939441996f586
rajLaha <priyanshulaha@gmail.com>
2024-05-19 13:06:44 +0530

    add multiple routes for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import {
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginuser,
   logoutuser,
   refreshAccessToken,
   registeruser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
 } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import { changeCurrentPassword } from "../controllers/user.controllers.js";
+import multer from "multer";
 
 const router = Router();
 
@@ -33,5 +40,19 @@ router.route("/logout").post(verifyJWT, logoutuser);
 router.route("/refresh-token").post(refreshAccessToken);
 
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
