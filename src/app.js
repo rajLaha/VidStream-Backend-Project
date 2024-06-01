47c1264b76edc7036780662caebc35ac08a5018a
rajLaha <priyanshulaha@gmail.com>
2024-06-01 14:05:09 +0530

    Add endpoint for video route

--- src/app.js
+++ src/app.js
@@ -20,11 +20,12 @@ app.use(cookieParser());
 // routes
 
 import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
 
 // routes declarations
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
 
 // http://localhost:8000/api/v1/usesrs/register
 
 export { app };
-
