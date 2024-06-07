c07b7cb79cc31d04f9e6d29a2d710ef98eb3f6bd
rajLaha <priyanshulaha@gmail.com>
2024-06-07 23:41:11 +0530

    add routes for comment

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -4,6 +4,10 @@ import {
   addComment,
   deleteComment,
   updateComment,
+  addPostComment,
+  deletePostComment,
+  updatePostComment,
+  getPostComments,
 } from "../controllers/comment.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -12,6 +16,11 @@ const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/:videoId").get(getVideoComments).post(addComment);
+router.route("/p/:postId").get(getPostComments).post(addPostComment);
 router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
+router
+  .route("/c/p/:commentId")
+  .delete(deletePostComment)
+  .patch(updatePostComment);
 
 export default router;
