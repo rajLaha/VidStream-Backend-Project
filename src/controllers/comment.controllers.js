5a1561d8742ae3f846113cc040193df98ec5535d
rajLaha <priyanshulaha@gmail.com>
2024-06-25 00:57:20 +0530

    Add try catch block in most of controllers of Comment

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -2,7 +2,7 @@ import mongoose, { isValidObjectId } from "mongoose";
 import { User } from "../models/user.models.js";
 import { Comment } from "../models/comment.models.js";
 import { Video } from "../models/video.models.js";
-import { ApiError } from "../utils/apiError.js";
+import { ApiError, catchError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Post } from "../models/post.models.js";
@@ -22,87 +22,96 @@ const addComment = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   const { content } = await req.body;
 
-  if (!videoId) {
-    throw new ApiError(401, "Unauthorized Access");
+  try {
+    if (!videoId) {
+      throw new ApiError(400, "Required URL parameter is missing videoId");
+    }
+
+    const isVideoExists = await Video.findById(videoId);
+
+    if (!isVideoExists) {
+      throw new ApiError(404, "Video not found");
+    }
+
+    if (!content) {
+      throw new ApiError(400, "Comment is Required");
+    }
+
+    const comment = await Comment.create({
+      content,
+      video: isVideoExists._id,
+      owner: req.user?._id,
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, comment, "Comment added Succesfully"));
+  } catch (error) {
+    catchError(error, res, "Adding Comment in Video");
   }
-
-  const isVideoExists = await Video.findById(videoId);
-
-  if (!isVideoExists) {
-    throw ApiError(404, "Video not found");
-  }
-
-  if (!content) {
-    throw new ApiError(400, "Comment is Required");
-  }
-
-  const comment = await Comment.create({
-    content,
-    video: isVideoExists._id,
-    owner: req.user?._id,
-  });
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, comment, "Comment added on video Succesfully"));
 });
 
 const updateComment = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
-
-  const comment = await Comment.findById(commentId);
-
-  if (!commentId) {
-    throw new ApiError(404, "Comment not found");
-  }
-
   const { newComment } = req.body;
 
-  if (!newComment) {
-    throw new ApiError(400, "Comment is required");
+  try {
+    if (!commentId) {
+      throw new ApiError(400, "Required URL parameter is missing commentId");
+    }
+
+    if (!newComment) {
+      throw new ApiError(400, "Comment is Mandatory");
+    }
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      throw new ApiError(404, "Comment not found");
+    }
+
+    if (req.user?._id.toString() != comment.owner.toString()) {
+      throw new ApiError(401, "Unauthorized User Access");
+    }
+
+    comment.content = newComment;
+    const cm = await comment.save();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, cm, "Comment Updated Succesfully"));
+  } catch (error) {
+    catchError(error, res, "Updating video comment");
   }
-
-  const user = req.user?._id.toString();
-  const commentIdOwner = comment.owner.toString();
-
-  if (user != commentIdOwner) {
-    throw new ApiError(401, "Unauthorized User Access");
-  }
-
-  comment.content = newComment;
-  const cm = await comment.save();
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, cm, "Comment Updated Succesfully"));
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
 
-  if (!commentId) {
-    throw new ApiError(404, "Unauthorized user access");
+  try {
+    if (!commentId) {
+      throw new ApiError(400, "Required URL parameter is missing commentId");
+    }
+
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      throw new ApiError(404, "Comment not found");
+    }
+
+    if (req.user?._id.toString() != comment.owner.toString()) {
+      throw new ApiError(401, "Unauthorized user access");
+    }
+
+    await Comment.deleteOne({
+      _id: commentId,
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Comment Delete Succesfully"));
+  } catch (error) {
+    catchError(error, res, "Deleting Comment from Video");
   }
-
-  const comment = await Comment.findById(commentId);
-  if (!comment) {
-    throw new ApiError(404, "Comment not found");
-  }
-
-  const currentUser = req.user?._id.toString();
-  const commentOwner = comment.owner.toString();
-
-  if (currentUser != commentOwner) {
-    throw new ApiError(401, "Unauthorized user access");
-  }
-
-  await Comment.deleteOne({
-    _id: commentId,
-  });
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Video Delete Succesfully"));
 });
 
 const getPostComments = asyncHandler(async (req, res) => {
@@ -120,90 +129,97 @@ const addPostComment = asyncHandler(async (req, res) => {
   const { postId } = req.params;
   const { content } = await req.body;
 
-  if (!postId) {
-    throw new ApiError(401, "Unauthorized Access");
+  try {
+    if (!postId) {
+      throw new ApiError(400, "Required URL parameter is missing postId");
+    }
+
+    if (!content) {
+      throw new ApiError(400, "Comment is Mandatory");
+    }
+
+    const isPostExists = await Post.findById(postId);
+
+    if (!isPostExists) {
+      throw ApiError(404, "Post not found");
+    }
+
+    const comment = await Comment.create({
+      content,
+      post: isPostExists._id,
+      owner: req.user?._id,
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, comment, "Comment added Succesfully"));
+  } catch (error) {
+    catchError(error, res, "Adding Comment in Post");
   }
-
-  const isPostExists = await Post.findById(postId);
-
-  if (!isPostExists) {
-    throw ApiError(404, "Post not found");
-  }
-
-  if (!content) {
-    throw new ApiError(400, "Comment is Required");
-  }
-
-  const comment = await Comment.create({
-    content,
-    post: isPostExists._id,
-    owner: req.user?._id,
-  });
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, comment, "Comment added on Post Succesfully"));
 });
 
 const updatePostComment = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
-
-  const comment = await Comment.findById(commentId);
-
-  if (!commentId) {
-    throw new ApiError(404, "Comment not found");
-  }
-
   const { newComment } = req.body;
 
-  if (!newComment) {
-    throw new ApiError(400, "Comment is required");
+  try {
+    if (!commentId) {
+      throw new ApiError(400, "Requierd URL paramater is missing commentId");
+    }
+
+    if (!newComment) {
+      throw new ApiError(400, "Comment is Mandatory");
+    }
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      throw new ApiError(404, "Comment not found");
+    }
+
+    if (req.user?._id.toString() != comment.owner.toString()) {
+      throw new ApiError(401, "Unauthorized User Access");
+    }
+
+    comment.content = newComment;
+    const cm = await comment.save();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, cm, "Comment Updated Succesfully"));
+  } catch (error) {
+    catchError(error, res, "Updating Post Comment");
   }
-
-  const user = req.user?._id.toString();
-
-  console.log(user, req.user._id);
-
-  const commentIdOwner = comment.owner.toString();
-
-  if (user != commentIdOwner) {
-    throw new ApiError(401, "Unauthorized User Access");
-  }
-
-  comment.content = newComment;
-  const cm = await comment.save();
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, cm, "Comment Updated Succesfully"));
 });
 
 const deletePostComment = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
 
-  if (!commentId) {
-    throw new ApiError(404, "Unauthorized user access");
+  try {
+    if (!commentId) {
+      throw new ApiError(400, "Required URL paramater is missing commentId");
+    }
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      throw new ApiError(404, "Comment not found");
+    }
+
+    if (req.user?._id.toString() != comment.owner.toString()) {
+      throw new ApiError(401, "Unauthorized user access");
+    }
+
+    await Comment.deleteOne({
+      _id: commentId,
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Comment Delete Succesfully"));
+  } catch (error) {
+    catchError(error, res, "Deleting Post Comment");
   }
-
-  const comment = await Comment.findById(commentId);
-  if (!comment) {
-    throw new ApiError(404, "Comment not found");
-  }
-
-  const currentUser = req.user?._id.toString();
-  const commentOwner = comment.owner.toString();
-
-  if (currentUser != commentOwner) {
-    throw new ApiError(401, "Unauthorized user access");
-  }
-
-  await Comment.deleteOne({
-    _id: commentId,
-  });
-
-  return res
-    .status(200)
-    .json(new ApiResponse(200, {}, "Post Delete Succesfully"));
 });
 
 export {
