a60e2a5421e91ae0d3ecbbef2f670158e6fc34fe
rajLaha <priyanshulaha@gmail.com>
2024-12-18 01:04:12 +0530

    Fix the limit in app.js

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "1024mb" }));
-app.use(express.urlencoded({ extended: true, limit: "1024mb" }));
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ extended: true, limit: "500mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
