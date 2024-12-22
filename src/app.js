c9359066cc0b7d9facd79404256c6493d321321c
rajLaha <priyanshulaha@gmail.com>
2024-12-22 14:53:59 +0530

    adding changes in app.js file

--- src/app.js
+++ src/app.js
@@ -12,7 +12,14 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: '100mb' }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use(express.json({ limit: '110mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
