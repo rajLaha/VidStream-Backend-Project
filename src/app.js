d87ee0e84696bfc423aa173dd700f81517760ae4
rajLaha <priyanshulaha@gmail.com>
2024-12-22 18:10:15 +0530

    ......

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://rajlaha.github.io');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, DELETE, OPTIONS, PATCH'
